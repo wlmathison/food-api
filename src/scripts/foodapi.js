595cb8861a1de1d2e0bc9cc9ec7bca6442d7d4ce
Billy Mathison <wlmathison@gmail.com>
2019-03-08 11:25:40 -0600

    input external api fetch call and adding product info into dom

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -5,22 +5,35 @@
 const container = document.querySelector("article")
 container.className = "foodList"
 const foodFactory = (foods) => {
-    return `<h2>${foods.name}</h2><p>${foods.ethnicity}</p><p>${foods.category}</p>`
+    return `<h2>${foods.name}</h2><p>Ethnicity: ${foods.ethnicity}</p><p>Category: ${foods.category}</p><p>Ingredients: ${foods.ingredients}</p><p>Country of Origin: ${foods.countryOrigin}</p><p>Calories: ${foods.caloriesPerServing}</p><p>Fat: ${foods.fatPerServing}</p><p>Sugars: ${foods.sugarPerServing}</p>`
 }
 
 const addFoodToDom = (foods) => {
     const foodListSection = document.createElement("section")
     foodListSection.innerHTML = ""
     foodListSection.innerHTML += foods
-    container.appendChild(foodListSection)  
+    container.appendChild(foodListSection)
 }
 
 fetch("http://localhost:8088/food")
     .then(foods => foods.json())
     .then(parsedFoods => {
-        console.table(parsedFoods)
         parsedFoods.forEach(foods => {
-            const foodAsHTML = foodFactory(foods)
-            addFoodToDom(foodAsHTML)
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${foods.barcode}.json`)
+                .then(response => response.json())
+                .then(productInfo => {
+                    foods.ingredients = productInfo.product.ingredients_text
+                    foods.countryOrigin = productInfo.product.purchase_places
+                    foods.caloriesPerServing = productInfo.product.nutriments.energy
+                    foods.fatPerServing = productInfo.product.nutriments.fat
+                    foods.sugarPerServing = productInfo.product.nutriments.sugars
+
+                    // Produce HTML representation
+                    const foodAsHTML = foodFactory(foods)
+
+                    // Add representaiton to DOM
+                    addFoodToDom(foodAsHTML)
+                })
+
         })
     })
